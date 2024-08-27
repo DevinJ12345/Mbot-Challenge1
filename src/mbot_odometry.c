2e3a0c5b6214fef305248ca5b95bc43fc29f98e7
Xiaoxiao Du <xiaoxiaodu@Xiaoxiaos-MacBook-Air-4.local>
2024-08-27 15:48:32 -0400

    init update: 2240827

---
+++ src/mbot_odometry.c
@@ -0,0 +1,8 @@
+#include "mbot_odometry.h"
+
+int mbot_calculate_odometry(serial_twist2D_t mbot_vel, float dt, serial_pose2D_t *odometry){
+   
+   // TO DO: Fill in calculate odometry
+   
+    return 0; // Return 0 to indicate success
+}
